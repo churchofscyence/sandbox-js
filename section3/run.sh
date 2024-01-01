c2fe8ff44ecff1e527857c6142b15ec9c37e423b
Johnny Cowboy <johnnycowboy3033@gmail.com>
2024-01-01 16:50:28 -0500

    Section3

---
+++ section3/run.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/bash
+echo "Section 3 : Call Back"
+
+npm run start
---
+++ section3/src/index.js
@@ -0,0 +1,18 @@
+function myCalculator(num1, num2, myCallback) {
+    let sum = num1 + num2;
+    myCallback(sum);
+}
+
+function print(number) {
+    console.log("Just Print: "+ number);
+}
+
+myCalculator(5, 5, print);
+
+function increaseByOnePrint(number) {
+
+    number = number + 1;
+    console.log("Increase By One Then Print: " + number);
+}
+
+myCalculator(5, 5, increaseByOnePrint);
